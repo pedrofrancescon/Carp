a40cfd3a151c369655bf5aff9db80a90c954ab44
Pedro Francescon Cittolin <pfcittolin@gmail.com>
2018-09-22 12:31:51 -0300

    Some coding organization

--- Carp/View/SlidingView.swift
+++ Carp/View/SlidingView.swift
@@ -64,13 +64,9 @@ class SlidingView: TouchesPassThroughView {
     
     // MARK: - Animation
     
-    /// The current state of the animation. This variable is changed only when an animation completes.
     var currentState: SlidingViewState = .destiny
+        private var runningAnimators = [UIViewPropertyAnimator]()
     
-    /// All of the currently running animators.
-    private var runningAnimators = [UIViewPropertyAnimator]()
-    
-    /// The progress of each animator. This array is parallel to the `runningAnimators` array.
     private var animationProgress = [CGFloat]()
     
     private lazy var panRecognizer: UIPanGestureRecognizer = {
@@ -80,14 +76,10 @@ class SlidingView: TouchesPassThroughView {
     }()
 
     
-    /// Animates the transition, if the animation is not already running.
     private func animateTransitionIfNeeded(to state: SlidingViewState, duration: TimeInterval) {
         
-        // ensure that the animators array is empty (which implies new animations need to be created)
         guard runningAnimators.isEmpty else { return }
-        
-        // an animator for the transition
-        let transitionAnimator = UIViewPropertyAnimator(duration: duration, dampingRatio: 1, animations: {
+                let transitionAnimator = UIViewPropertyAnimator(duration: duration, dampingRatio: 1, animations: {
             switch state {
             case .origin:
                 self.destinyLeadingConstraint.constant = self.originStateConstraint
@@ -101,7 +93,6 @@ class SlidingView: TouchesPassThroughView {
             self.superview?.layoutIfNeeded()
         })
         
-        // the transition completion block
         transitionAnimator.addCompletion { position in
             
             // update the state
@@ -114,7 +105,6 @@ class SlidingView: TouchesPassThroughView {
                 ()
             }
             
-            // manually reset the constraint positions
             switch self.currentState {
             case .origin:
                 self.destinyLeadingConstraint.constant = self.originStateConstraint
@@ -126,15 +116,12 @@ class SlidingView: TouchesPassThroughView {
                 self.destinyLeadingConstraint.constant = self.hiddenStateConstraint
             }
             
-            // remove all running animators
             self.runningAnimators.removeAll()
             
         }
         
-        // start all animators
         transitionAnimator.startAnimation()
         
-        // keep track of all running animators
         runningAnimators.append(transitionAnimator)
         
     }
@@ -143,43 +130,34 @@ class SlidingView: TouchesPassThroughView {
         switch recognizer.state {
         case .began:
             
-            // start the animations
             animateTransitionIfNeeded(to: currentState.opposite, duration: 1)
             
-            // pause all animations, since the next event may be a pan changed
             runningAnimators.forEach { $0.pauseAnimation() }
             
-            // keep track of each animator's progress
             animationProgress = runningAnimators.map { $0.fractionComplete }
             
         case .changed:
             
-            // variable setup
             let translation = recognizer.translation(in: self)
             var fraction = -translation.x / self.originStateConstraint
             
-            // adjust the fraction for the current state and reversed state
             if currentState == .destiny { fraction *= -1 }
             if runningAnimators[0].isReversed { fraction *= -1 }
             
-            // apply the new fraction
             for (index, animator) in runningAnimators.enumerated() {
                 animator.fractionComplete = fraction + animationProgress[index]
             }
             
         case .ended:
             
-            // variable setup
             let xVelocity = recognizer.velocity(in: self).x
             let shouldClose = xVelocity > 0
             
-            // if there is no motion, continue all animations and exit early
             if xVelocity == 0 {
                 runningAnimators.forEach { $0.continueAnimation(withTimingParameters: nil, durationFactor: 0) }
                 break
             }
             
-            // reverse the animations based on their current state and pan motion
             switch currentState {
             case .origin:
                 if !shouldClose && !runningAnimators[0].isReversed { runningAnimators.forEach { $0.isReversed = !$0.isReversed } }
@@ -191,7 +169,6 @@ class SlidingView: TouchesPassThroughView {
                 break
             }
             
-            // continue all animations
             runningAnimators.forEach { $0.continueAnimation(withTimingParameters: nil, durationFactor: 0) }
             
         default:
